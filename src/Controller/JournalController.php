70d73d52368dd35d78b777a2c48f608593d4f06f
Michael Ihde <michitheonlyone@gmail.com>
2023-08-07 10:21:15 +0200

    added edit function

--- src/Controller/JournalController.php
+++ src/Controller/JournalController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\JournalEntry;
 use App\Form\JournalEntryFormType;
 use App\Repository\JournalEntryRepository;
 use Doctrine\Common\Collections\Criteria;
@@ -21,6 +22,37 @@ class JournalController extends AbstractController
             $newEntry = $form->getData();
             $newEntry->setTimestamp(new \DateTime('now'));
             $journalEntryRepository->save($newEntry,true);
+            return $this->redirectToRoute('journal');
+        }
+
+        // TODO replace with components!!! or replace form with components and ajax stimulus inserting and editting!
+        if ($request->get('all')) {
+            $journal = $journalEntryRepository->findAll();
+        } else {
+            $journalDateCriteria = new Criteria();
+            $journalDateCriteria->where(Criteria::expr()->gt('timestamp', [(new \DateTimeImmutable('now'))->format('Y-m-d')]));
+            $journalDateCriteria->andWhere(Criteria::expr()->lt('timestamp', [(new \DateTimeImmutable('now +1 day'))->format('Y-m-d')]));
+            $journal = $journalEntryRepository->matching($journalDateCriteria);
+        }
+
+        $env = str_replace('sqlite:///%kernel.project_dir%/var/', '', $_ENV['DATABASE_URL']);
+
+        return $this->render('journal.html.twig', ['journal_entry_form' => $form, 'journal_entries' => $journal, 'db' => $env]);
+    }
+
+    #[Route('/edit/{id}', name: 'entry.edit')]
+    public function edit(int $id, JournalEntryRepository $journalEntryRepository, Request $request): Response
+    {
+        $entry = $journalEntryRepository->find($id);
+        if (!$entry instanceof JournalEntry) {
+            return $this->redirectToRoute('journal');
+        }
+
+        $form = $this->createForm(JournalEntryFormType::class, $entry);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entry = $form->getData();
+            $journalEntryRepository->save($entry,true);
             return $this->redirectToRoute('journal');
         }
 
